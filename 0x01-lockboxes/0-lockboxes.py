816119f844d12e776a119de22bc7b5e14c7c5d44
mawusikpodo <mawusikpodo@yahoo.com>
2024-01-09 18:01:13 +0000

    added solution

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -20,8 +20,3 @@ def canUnlockAll(boxes):
     
     # Check if all boxes are opened
     return len(opened_boxes) == len(boxes)
-
-# Example usage:
-boxes = [[1], [2], [3], []]
-result = canUnlockAll(boxes)
-print(result)  # Output: True
