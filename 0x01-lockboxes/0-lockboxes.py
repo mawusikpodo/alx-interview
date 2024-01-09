cf4f37f06d49ed94f78bb04b31e75dfc94bc4ccf
mawusikpodo <mawusikpodo@yahoo.com>
2024-01-09 18:08:21 +0000

    added solution

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,39 @@
 #!/usr/bin/python3
 
-def canUnlockAll(boxes):
+def can_unlock_all(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+    - boxes (list of lists): A list where each element is a list of keys present in a box.
+
+    Returns:
+    - bool: True if all boxes can be opened, False otherwise.
+    """
     # Set to keep track of opened boxes
     opened_boxes = set()
-    
-    # DFS function to explore the boxes
+
     def dfs(box_number):
+        """
+        Depth-first search to explore the boxes.
+
+        Args:
+        - box_number (int): The current box number to explore.
+        """
         # If the box is already opened, return
         if box_number in opened_boxes:
             return
-        
+
         # Mark the box as opened
         opened_boxes.add(box_number)
-        
+
         # Explore the keys in the current box
         for key in boxes[box_number]:
             dfs(key)
-    
+
     # Start DFS from the first box
     dfs(0)
-    
+
     # Check if all boxes are opened
     return len(opened_boxes) == len(boxes)
+
